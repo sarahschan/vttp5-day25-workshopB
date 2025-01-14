f989470b1862d04813eb8299b6dfe8b96922f57a
sarahschan <sarahschan@hotmail.com>
2025-01-14 13:33:56 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/paf_day25_wsB/PafDay25WsBApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_day25_wsB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafDay25WsBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafDay25WsBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/paf_day25_wsB/PafDay25WsBApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_day25_wsB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafDay25WsBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
